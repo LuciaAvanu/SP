0f2f6c917e32ff844b330a68dc50b9c1e39d8591
Lucia Avanu <avanulucia.miruna@yahoo.com>
2019-12-25 16:58:59 +0200

    Update T1_ex1_Avanu_Lucia.m

--- T1_ex1_Avanu_Lucia.m
+++ T1_ex1_Avanu_Lucia.m
@@ -11,7 +11,7 @@ title('Semnal dreptunghiular periodic, rezolutie temporara 2ms')        %setam t
 
 
 figure(2)
-%generam o perioada de 2s al unui semnal, cu rezolutia temporara de 2 ms
+%generam o perioada de 2s al unui semnal, cu rezolutia temporara de 20 ms
 t=0:0.02:2;
 %generam semnalul dreptunghiular, cu factorul de umplere 25%
 semnal=-0.25+0.75*square(t,25);   %componenta continua -0.25 si amplitudinea 0.75 pentru ca semnalul sa se intinda la nivel maxim=0.5 si minim -1
@@ -26,7 +26,7 @@ title('Semnal dreptunghiular periodic, rezolutie temporara 20ms')    %setam titl
 
 
 figure(3)
-%generam o perioada de 2s al unui semnal, cu rezolutia temporara de 2 ms
+%generam o perioada de 2s al unui semnal, cu rezolutia temporara de 200 ms
 t=0:0.2:2;
 %generam semnalul dreptunghiular, cu factorul de umplere 25%
 semnal=-0.25+0.75*square(t,25);      %componenta continua -0.25 si amplitudinea 0.75 pentru ca semnalul sa se intinda la nivel maxim=0.5 si minim -1
